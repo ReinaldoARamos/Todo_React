531f9eea1399180a8e7c3a89866dcad915d19661
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-22 23:14:43 -0300

    criando e estilizando o header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,16 +1,23 @@
-import styles from '../componentes/Header.module.css'
-import logo from '../assets/Logo.svg'
+import { PlusCircle} from "phosphor-react";
+import styles from "../componentes/Header.module.css";
+import logo from "../assets/Logo.svg";
 
- export function Header(header){
 
-    return(
-        <div>
-            <header className={styles.header}> <img src={logo} /></header>
-       
-       </div>
-           
-            
-      
-    )
+export function Header() {
+  return (
+    <div>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} />
+      </header>
+      <div className={styles.createTask}>
+      <input className={styles.input} placeholder={"Adicione uma tarefa aqui"} />
+      <button  className={styles.button}>
+        
+        Criar
+        
+        </button>
+      </div>
+     
+    </div>
+  );
 }
-
