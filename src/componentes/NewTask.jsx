2346b43fee969eb3fa3fb0470bb5179a9f9ad882
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-24 16:47:57 -0300

    hora de criar as props e colocar o valor nos componentes

--- src/componentes/NewTask.jsx
+++ src/componentes/NewTask.jsx
@@ -1,18 +1,20 @@
 import styles from "../componentes/NewTask.module.css";
 
-import { Circle } from "phosphor-react";
+import { Circle, Trash} from "phosphor-react";
 export function NewTask() {
   return (
     <div>
       <div className={styles.Task}>
-      <span><Circle className={styles.circle} size={20} /></span>
-        <span >
-       
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        <span>
+          <Circle className={styles.circle} size={20} />
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
           mollitia, molestiae quas vel sint commodi repudiandae consequuntur
           voluptatum laborum
+        
         </span>
+        <Trash size={20} />
       </div>
     </div>
   );
